17d6e44402cc504aefae8574df4706b3df5a9a41
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-27 10:09:31 +0500

    Finally chaanges

--- connect/src/Pages/HomePage/Sidebar/sidebar.jsx
+++ connect/src/Pages/HomePage/Sidebar/sidebar.jsx
@@ -2,8 +2,15 @@
 import { FaHome, FaUserFriends, FaBell, FaEnvelope } from 'react-icons/fa';
 import './Sidebar.css'; // Import the Sidebar CSS file
 import SidebarOption from './options/options'; // Adjust based on actual file name
+import { Navigate,useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+	const navigate = useNavigate();
+
+	const logout = () => {
+		localStorage.clear(); // This will clear all items in local storage
+		navigate('/login');
+	};
 	return (
 		<div className='sidebar'>
 			<h1 className='sidebar__twitterIcon'>Mizuka Connect</h1>
@@ -11,7 +18,9 @@ const Sidebar = () => {
 			<SidebarOption text='Friends' Icon={FaUserFriends} />
 			<SidebarOption text='Notifications' Icon={FaBell} />
 			<SidebarOption text='Messages' Icon={FaEnvelope} />
-			<button className='sidebar__tweet'>Post</button>
+			<button className='sidebar__tweet' onClick={logout}>
+				Logout
+			</button>
 		</div>
 	);
 };
