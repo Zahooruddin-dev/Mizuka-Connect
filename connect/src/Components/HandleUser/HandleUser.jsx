06eada75821f37c46a68d6da8a8b611de0bae45d
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-10-30 11:35:38 +0500

    styling for auth and stuff improved user

--- connect/src/Components/HandleUser/HandleUser.jsx
+++ connect/src/Components/HandleUser/HandleUser.jsx
@@ -1,25 +1,20 @@
 import { Link, Outlet, Navigate } from 'react-router-dom';
-import './AuthLayout.css'; // Import the CSS file for styling';
+import './AuthLayout.css';
 
 function AuthLayout() {
-	const isLoggedIn = localStorage.getItem('loggedin'); // Check if user is logged in
+	const isLoggedIn = localStorage.getItem('loggedin');
 
 	if (isLoggedIn) {
-		// If logged in, redirect to the homepage
 		return <Navigate to="/home" />;
 	}
 
 	return (
-		<div className='auth-container'>
+		<div className="auth-container">
 			<h1>Welcome to Mizuka</h1>
-
-			{/* Links to switch between Login and Signup */}
-			<div className='auth-links'>
-				<Link to='/login'>Login</Link>
-				<Link to='/signup'>Sign Up</Link>
+			<div className="auth-links">
+				<Link to="/login">Login</Link>
+				<Link to="/signup">Sign Up</Link>
 			</div>
-
-			{/* Outlet will render either Login or Signup based on the route */}
 			<Outlet />
 		</div>
 	);
